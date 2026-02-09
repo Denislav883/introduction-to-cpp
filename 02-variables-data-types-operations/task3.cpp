6f13bac6faefde89a7009e2d797b6fd7c2c804a7
Denislav883 <yanev.denislav@gmail.com>
2026-02-09 20:04:36 +0200

    Add C++ program to swap two variables using bitwise operations

---
+++ 02-variables-data-types-operations/task3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter first integer: ";
+    cin >> a;
+
+    cout << "Enter second integer: ";
+    cin >> b;
+
+    cout << "Before swapping:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+
+    cout << "After swapping:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
